307245a37e034d19320f612115d435b2beeecee7
Pradeep Kishore Gowda <pradeep@btbytes.com>
2012-02-24 18:11:57 -0500

    combinepdf usage

--- combinepdf.sh
+++ combinepdf.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# Usage: ./combinepdf.sh `ls -1` to combine all the pdfs in curdir into combine.pdf
+# source: http://www.debianadmin.com/combine-multiple-pdfs-into-one-file-in-ubuntu-linux.html
+# 2012-02-24
+
 gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=combined.pdf -dBATCH $@
